74f5b225ad4a1979c3571be6721ce29329b5021d
ijon9 <ijon@stuy.edu>
2019-03-31 21:56:51 -0400

    s

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ edges = []
 polygons = []
 transform = new_matrix()
 
-parse_file( 'myScript', edges, polygons, transform, screen, color )
+parse_file( 'script', edges, polygons, transform, screen, color )
